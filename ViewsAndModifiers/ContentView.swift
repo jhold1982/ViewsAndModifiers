d92beceb4f33c36767aff6df5afd9f0736f8018a
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-22 14:27:59 -0600

    Added custom view modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,13 +7,61 @@
 
 import SwiftUI
 
+struct CustomButtonModifier: ViewModifier {
+	func body(content: Content) -> some View {
+		content
+			.font(.largeTitle)
+			.foregroundColor(.white)
+			.padding()
+			.background(.blue)
+			.clipShape(RoundedRectangle(cornerRadius: 10))
+	}
+}
+
+extension View {
+	func customButton() -> some View {
+		modifier(CustomButtonModifier())
+	}
+}
+
+struct Watermark: ViewModifier {
+	var text: String
+	
+	func body(content: Content) -> some View {
+		ZStack(alignment: .bottomTrailing) {
+			content
+			Text(text)
+				.font(.caption)
+				.foregroundColor(.white)
+				.padding(5)
+				.background(.black)
+		}
+	}
+}
+
+extension View {
+	func watermarked(with text: String) -> some View {
+		modifier(Watermark(text: text))
+	}
+}
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
+			Spacer()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+			Spacer()
+			Color.blue
+				.frame(width: 300, height: 200)
+				.watermarked(with: "@leftHandedApps")
+				.padding()
+			Spacer()
+			Button("Continue", action: {})
+				.customButton()
         }
         .padding()
     }
@@ -24,3 +72,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
