8275aae153bb41d6992cb47cfd817a23de819e88
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-22 08:28:56 -0600

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Justin Hold on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
